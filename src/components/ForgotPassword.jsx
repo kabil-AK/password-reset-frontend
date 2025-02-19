86415b61bd50c049e0e7e3b803ae8faad7dcf7c9
kabil-AK <kabilanrajini007@gamil.com>
2025-02-19 05:55:41 +0530

    changes forget

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const navigate = useNavigate();
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
